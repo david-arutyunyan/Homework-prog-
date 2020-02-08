0beb1d9f65d462241d5b7fc54edde8852b655b76
david-arutyunyan <55546684+david-arutyunyan@users.noreply.github.com>
2020-02-08 10:02:27 +0300

    ДЗ #2(с изменениями)
    ДЗ #2(с изменениями)

--- ConsoleApp1/Calculator.cs
+++ ConsoleApp1/Calculator.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 
 namespace ConsoleApp1
 {
+    public delegate void ErrorNotificationType(string message);
+
     class Calculator
     {
         public delegate double MathOperation(double a, double b);
 
-        static Dictionary<char, MathOperation> operations;
+        public event ErrorNotificationType ErrorNotification;
+
+        public static Dictionary<char, MathOperation> operations;
 
         static Calculator()
         {
@@ -21,16 +25,9 @@ namespace ConsoleApp1
             };
         }
 
-        /// <summary>
-        /// Метод принимает строку с примером, разделяет её на числа и операцию(знак)
-        /// и возвращает результат
-        /// </summary>
-        /// <param name="expr">Строка с прмерчиком</param>
-        /// <returns>Ответ примера</returns>
-        public static double Calculate(string expr)
+        public void Invoke(string message)
         {
-            string[] a = expr.Split(' ');
-            return operations[a[1].ToCharArray()[0]](Convert.ToDouble(a[0]), Convert.ToDouble(a[2]));
+            ErrorNotification?.Invoke(message);
         }
     }
 }
